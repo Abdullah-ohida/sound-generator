a4fce17827296d116669aba996a35dd73d9172c6
Abdullah-ohida <ohida2001@gmail.com>
2020-04-21 21:47:45 +0100

    add onclick

--- main.js
+++ main.js
@@ -1,36 +1,34 @@
 
+
+
+
 function removeTransition(e) {
-    if (e.propertyName !== "transform") return;// skip it if it"s not a transform.
-    e.target.classList.remove("playing")
-}
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
 
-function playSound(e) {
+  function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if (!audio) return;// stop function from running all together.
-    audio.currentTime = 0;// Rewind to the start
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
     audio.play();
-    key.classList.add("playing");
-}
+  }
 
-const keys = Array.from(document.querySelectorAll(".key"));
-keys.forEach(key => key.addEventListener("transitionend", removeTransition));
-window.addEventListener('keydown', playSound);
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
 
-// function removeTransition(e) {
-//     if (e.propertyName !== 'transform') return;
-//     e.target.classList.remove('playing');
-//   }
 
-//   function playSound(e) {
-//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-//     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-//     if (!audio) return;a
-//     key.classList.add('playing');
-//     audio.currentTime = 0;
-//     audio.play();
-//   }
-
-//   const keys = Array.from(document.querySelectorAll('.key'));
-//   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-//   window.addEventListener('keydown', playSound);
+const ks = document.querySelectorAll(`.key`);
+ks.forEach((k)=>{
+ k.onclick = ()=>{
+  const audio = document.querySelector(`audio[data-key="${k.getAttribute('data-key')}"]`);
+  k.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+   
+ }
+})
